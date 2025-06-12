9452a13c91b05069cacf5a4bfc76bf57bdd65216
SFzde <sfdaniefranc3@gmail.com>
2025-06-11 22:42:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, jsonify
 from yt_dlp import YoutubeDL
+import os
 
 app = Flask(__name__)
+
+@app.before_first_request
+def create_cookie_file():
+    # Solo crea cookies.txt si no existe y la variable está definida
+    cookie_data = os.getenv("YOUTUBE_COOKIES")
+    if cookie_data and not os.path.exists("cookies.txt"):
+        with open("cookies.txt", "w", encoding='utf-8') as f:
+            f.write(cookie_data)
 
 @app.route('/get-audio-url')
 def get_audio_url():
@@ -13,6 +22,7 @@ def get_audio_url():
         'quiet': True,
         'format': 'bestaudio[ext=webm]/bestaudio',
         'skip_download': True,
+        'cookiefile': 'cookies.txt'
     }
 
     try:
@@ -22,7 +32,7 @@ def get_audio_url():
             return jsonify({'audioUrl': audio_url})
     except Exception as e:
         print(f"Error: {e}")
-        return 'No se pudo obtener la URL de audio {e}', 500
+        return 'No se pudo obtener la URL de audio', 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
